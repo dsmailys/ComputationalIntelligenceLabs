b09279777b73c6a22efce8585e9639ffdaff58ed
BENTLEY\Deividas.Smailys <deividas.smailys@bentley.com>
2018-03-04 15:01:52 +0200

    First categorical data visualisation

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,4 +1,7 @@
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
 from CampaignDetails import CampaignDetails
 import Constants
 
@@ -11,8 +14,26 @@ def ReadInitialData(dataFile):
         for line in reader:
             data_list.append(CampaignDetails(line))
     return data_list
+
+def DrawBarChart(objectList, dataList, yLabel, title):
+    y_pos = np.arange(len(objectList))
+    
+    plt.bar(y_pos, dataList, align='center', alpha=0.5)
+    plt.xticks(y_pos, objectList)
+    plt.ylabel(yLabel)
+    plt.title(title)
+    
+    plt.show()
     
 def __main__():
+    #read initial data set
     data = ReadInitialData(Constants.DATA_FILE)
+
+    # calculate categorical value for credit card holder
+    cardHolderCount = sum(1 for item in data if item.CC_CARD == '1')
+    nonCardHolder = sum(1 for item in data if item.CC_CARD == '0')
+    DrawBarChart(("CreditCard", "Cash"), (cardHolderCount, nonCardHolder), "Usage", "Credit card usage")
+
+    
 
 __main__()
