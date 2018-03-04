832c3e440f8b78dfdc38aafd36fac08005b6953c
dsmailys <smailys.deividas@gmail.com>
2018-03-04 15:24:50 +0200

    Second categorical value plot. Is a customer a web shopper?

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -15,13 +15,14 @@ def ReadInitialData(dataFile):
             data_list.append(CampaignDetails(line))
     return data_list
 
-def DrawBarChart(objectList, dataList, yLabel, title):
-    y_pos = np.arange(len(objectList))
-    plt.bar(y_pos, dataList, align='center', alpha=0.5)
-    plt.xticks(y_pos, objectList)
-    plt.ylabel(yLabel)
-    plt.title(title)
-    plt.show()
+def DrawBarChart(objectList, dataList, yLabel, title, showPlot = True):
+    if showPlot == True:
+        y_pos = np.arange(len(objectList))
+        plt.bar(y_pos, dataList, align='center', alpha=0.5)
+        plt.xticks(y_pos, objectList)
+        plt.ylabel(yLabel)
+        plt.title(title)
+        plt.show()
     
 def __main__():
     #read initial data set
@@ -30,6 +31,11 @@ def __main__():
     # calculate categorical value for credit card holder
     cardHolderCount = sum(1 for item in data if item.CC_CARD == '1')
     nonCardHolder = sum(1 for item in data if item.CC_CARD == '0')
-    DrawBarChart(("CreditCard", "Cash"), (cardHolderCount, nonCardHolder), "Usage", "Credit card usage")
+    DrawBarChart(("CreditCard", "Cash"), (cardHolderCount, nonCardHolder), "Usage", "Credit card usage", False)
+
+    # calculate categorical value for web shoppers
+    webShopperCount = sum(1 for item in data if item.WEB == '1')
+    nonWebShopperCount = sum(1 for item in data if item.WEB == '0')
+    DrawBarChart(("Web Shopper", "Store Shopper"), (webShopperCount, nonWebShopperCount), "Usage", "Web shopping usage", False)
 
 __main__()
