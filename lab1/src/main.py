65db6891f3bfc5e6a5aa4e70d292bfaecd5e14e7
Matas Valužis <matas.valuzis@gmail.com>
2018-03-05 22:36:26 +0200

    added table to display stats about selected features added dependenvy management for pipenv

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import prettytable
 from decimal import Decimal
 
 from CampaignDetails import CampaignDetails
@@ -9,7 +10,7 @@ import Constants
 
 def ReadInitialData(dataFile):
     data_list = []
-    with open(dataFile, 'rb') as csv_file:
+    with open(dataFile, 'rt', encoding="ascii") as csv_file:
         reader = csv.reader(csv_file)
         next(reader, None)  # Skip the header.
         # Unpack the row directly in the head of the for loop.
@@ -44,7 +45,9 @@ def FindOutliers (dataList):
 
 def PrintList (dataList):
     for item in dataList:
-        print(item)
+        print (item)
+
+
 
 def FindMissingValues (dataList):
     missingCount = 0
@@ -52,68 +55,96 @@ def FindMissingValues (dataList):
         if not item:
             missingCount = missingCount + 1
     return missingCount
-    
+
+def PrintTable (headers, rows):
+    table = prettytable.PrettyTable(headers)
+    for row in rows:
+        table.add_row(row)
+    print (table)
+
+
+
 def __main__():
     #read initial data set
     data = ReadInitialData(Constants.DATA_FILE)
 
+    
+    cardHolderData = ["Card holder"]
+    webShopperData = ["Web shopper"]
+    avarageSpentData = ["Spent"]
+    promotionsRespondedData = ["Responded"]
+
     # calculate categorical value for credit card holder
     cardHolderCount = sum(1 for item in data if item.CC_CARD == '1')
     nonCardHolder = sum(1 for item in data if item.CC_CARD == '0')
+    cardHolderData.append(cardHolderCount + nonCardHolder)
     DrawBarChart(("CreditCard", "Cash"), (cardHolderCount, nonCardHolder), "Usage", "Credit card usage", True)
 
     # calculate categorical value for web shoppers
     webShopperCount = sum(1 for item in data if item.WEB == '1')
     nonWebShopperCount = sum(1 for item in data if item.WEB == '0')
+    webShopperData.append(nonWebShopperCount + webShopperCount)
     DrawBarChart(("Web Shopper", "Store Shopper"), (webShopperCount, nonWebShopperCount), "Usage", "Web shopping usage", True)
 
     # calculate numerical value for average amount spent per visit
-    spentSorted = list(float(item.AVRG) for item in data)
+    spentSorted = list(float(item.AVRG) for item in data if not item == "")
+    avarageSpentData.append(len(spentSorted))
     DrawHistogram(spentSorted, "Average amount spent per visit", "Frequency", "Amount spent", 500, [0, 300, 0, 800], True)
 
     #number of RESPONDED promotions last year
-    responded = list(int(item.RESPONDED) for item in data)
+    responded = list(int(item.RESPONDED) for item in data if not item == "")
+    promotionsRespondedData.append(len(responded))
     DrawHistogram(responded, "Number of responded promotions last year", "Frequency", "Responded amount", 30, [0, 10, 0, 12000], True)
 
     # finding outliers for average spent
     elements = list(float(item.AVRG) for item in data)
     outliers = FindOutliers (elements)
     print ("Outliers for average spent: ")
-    PrintList(outliers)
+    print(outliers)
 
     #finding outliers for promotions responded last year
     elements = list(int(item.RESPONDED) for item in data)
     outliers = FindOutliers (elements)
     print ("Outliers for promotions responded last year")
-    PrintList (outliers)
+    print (outliers)
 
     #finding missing values for average spent
-    missing = FindMissingValues ((item.AVRG for item in data))
-    print ("Missing data from average amount spent per visit:")
-    print (missing)
+    averageSpentMissing = FindMissingValues ((item.AVRG for item in data))
+    avarageSpentData.append(averageSpentMissing)    
 
     #finding missing values for promotions responded last year
     missing = FindMissingValues ((item.RESPONDED for item in data))
-    print ("Missing data from promotions responded last year")
-    print (missing)
+    promotionsRespondedData.append(missing)    
+
+    #finding missing values for web shoppers
+    missing = FindMissingValues ((item.WEB for item in data))
+    webShopperData.append(missing) 
+
+    #finding missing values for credit card holders
+    missing = FindMissingValues ((item.CC_CARD for item in data))
+    cardHolderData.append(missing) 
 
     # finding cardinality for average spent
-    avgSpent = list((item.AVRG for item in data))
-    avgSpentSet = set(avgSpent)
-    print("Average spent data set length:")
-    print(len(avgSpent))
-    print("Cardinality of values:")
-    print(len(avgSpentSet))
+    avgSpentSet = set((item.AVRG for item in data))
+    avarageSpentData.append(len(avgSpentSet)) 
 
     # finding cardinality for promotions responded
-    responded = list((item.RESPONDED for item in data))
-    respondedSet = set(responded)
-    print("Responded to promotions data set length:")
-    print(len(responded))
-    print("Cardinality of values:")
-    print(len(respondedSet))
+    respondedSet = set((item.RESPONDED for item in data))
+    promotionsRespondedData.append(len(respondedSet))
 
-# TODO: Do we need table and graphs for quality report?
+    # finding cardinality for web shoppers
+    webShopperData.append(2)
+
+    # finding cardinality for credit card holders
+    cardHolderData.append(2)
+
+    print("Categorical features:")
+    PrintTable(["Feature", "Count", "Miss. %", "Card."], [webShopperData, cardHolderData])
+
+    print("Continuous features:")    
+    PrintTable(["Feature", "Count", "Miss. %", "Card."], [promotionsRespondedData, avarageSpentData])
+
+
 # TODO: 
 # (3) explore the shape of distribution. Perform standardization and normalization, consider the normality of data 
 # 	- graphs and description of actions done
