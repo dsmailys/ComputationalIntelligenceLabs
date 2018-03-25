53e5ebedcffb88198ef7bb5e0d619ad8e67c47a6
dsmailys <smailys.deividas@gmail.com>
2018-03-25 22:05:11 +0300

    Decision tree and calculating accuracy

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,18 +1,18 @@
+#!python3
+
 import csv
 import random
 
 from CampaignDetails import CampaignDetails
 import Constants
 
-def ReadInitialData(dataFile):
-    data_list = []
-    with open(dataFile, 'rt') as csv_file:
-        reader = csv.reader(csv_file)
-        next(reader, None)  # Skip the header.
-        # Unpack the row directly in the head of the for loop.
-        for line in reader:
-            data_list.append(CampaignDetails(line))
-    return data_list
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+from sklearn.externals.six import StringIO  
+import pydot 
 
 def SplitDataByProportion (data, training_proportion, test_proportion):
     if training_proportion > 100 or training_proportion < 0:
@@ -24,19 +24,33 @@ def SplitDataByProportion (data, training_proportion, test_proportion):
     
     totalLength = len(data)
     trainingCount = training_proportion * totalLength / 100
-    trainingData = data[:trainingCount]
+    trainingData = data[:int(trainingCount)]
 
-    testData = data[trainingCount:totalLength]
+    testData = data[int(trainingCount):int(totalLength)]
     return trainingData, testData
 
-def __main__():
-    data = ReadInitialData(Constants.DATA_FILE)
-    random.shuffle(data)
+def __main__():  
+    df = pd.read_csv (Constants.DATA_FILE)
+    df = shuffle(df)
+    training_data, test_data = SplitDataByProportion(df, 90, 10)
     
-    training_data, test_data = SplitDataByProportion(data, 90, 10)
-    print(len(training_data))
-    print(len(test_data))
-    
+    training_data_projection = training_data[['RESPONDED', 'WEB', 'AVRG', 'CC_CARD']]
+    target_data_projection = training_data['PC_CALC20']
+
+    test_data_projection = test_data[['RESPONDED', 'WEB', 'AVRG', 'CC_CARD']]
+    actual_result_data = test_data['PC_CALC20']
+
+    clf_gini = DecisionTreeClassifier(criterion = "gini", max_depth=3, min_samples_leaf=5)
+    clf_gini.fit(training_data_projection, target_data_projection)
+    results = clf_gini.predict(test_data_projection)
+
+    accuracy = accuracy_score(actual_result_data, results)*100
+    print(accuracy)
+    dot_data = StringIO()
+    export_graphviz(clf_gini, out_file=dot_data, filled=True, rounded=True,  special_characters=True)
+    graph = pydot.graph_from_dot_data(dot_data.getvalue())
+    graph[0].write_png("iris.png") 
+
 # Lab2 Report link: https://docs.google.com/document/d/1SWtKXVoKqoz7XgTmn5he2f8zm2Z3e508Rk_c8y3y82M/edit?usp=sharing
 
 __main__()
