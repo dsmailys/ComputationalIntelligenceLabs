57f137db5a6319dc8667301be4cef845adb1e83f
dsmailys <smailys.deividas@gmail.com>
2018-03-25 16:10:04 +0300

    Shuffling data, spliting by 9:1 proportion and added Report to lab link (started filling it out)

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 from CampaignDetails import CampaignDetails
 import Constants
@@ -13,8 +14,29 @@ def ReadInitialData(dataFile):
             data_list.append(CampaignDetails(line))
     return data_list
 
+def SplitDataByProportion (data, training_proportion, test_proportion):
+    if training_proportion > 100 or training_proportion < 0:
+        raise ValueError("Training proportion must be between 0 and 100")
+    if test_proportion > 100 or test_proportion < 0:
+        raise ValueError("Test proportion must be between 0 and 100")
+    if test_proportion + training_proportion != 100:
+        raise ValueError("Test and training proportions must be equal to 100 when added")
+    
+    totalLength = len(data)
+    trainingCount = training_proportion * totalLength / 100
+    trainingData = data[:trainingCount]
+
+    testData = data[trainingCount:totalLength]
+    return trainingData, testData
+
 def __main__():
     data = ReadInitialData(Constants.DATA_FILE)
+    random.shuffle(data)
     
+    training_data, test_data = SplitDataByProportion(data, 90, 10)
+    print(len(training_data))
+    print(len(test_data))
+    
+# Lab2 Report link: https://docs.google.com/document/d/1SWtKXVoKqoz7XgTmn5he2f8zm2Z3e508Rk_c8y3y82M/edit?usp=sharing
 
 __main__()
